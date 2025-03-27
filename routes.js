9860f6445f8832eb08c0321ff9731ab2b0900662
Tu Nombre <jorge.romo.23s@utzmg.edu.mx>
2025-03-27 11:00:09 -0600

    segundo

--- routes.js
+++ routes.js
@@ -6,11 +6,13 @@ global.conection = "mongodb+srv://solutionhit128:Capibara124@cluster0.yddko.mong
 //QuintoEdgar
 //global.route = "http://192.168.0.163:8010";
 //2A 
-global.route = "http://10.198.56.86:8010";
+//global.route = "http://10.198.56.86:8010";
 //casa 
 //global.route = "http://192.168.100.46:8010";
 //5to
 //global.route = "http://192.168.0.186:8010"
 //Red Jalisco
 //global.route = "http://10.1.2.165:8010"
+// --> De Localhost
+global.route = "http://localhost:8010";
 
